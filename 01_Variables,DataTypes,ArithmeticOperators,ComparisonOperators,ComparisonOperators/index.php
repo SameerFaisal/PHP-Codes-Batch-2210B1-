93fa79a45238ab49d6aa27dbada5a1363e32b2c4
SameerFaisal <sameerfaisal46@gmail.com>
2023-07-26 11:15:04 +0500

    Codes done on 26-07-2023

---
+++ 01_Variables,DataTypes,ArithmeticOperators,ComparisonOperators,ComparisonOperators/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+<?php
+echo "Class 1 - PHP"
+
+?>
+    </title>
+</head>
+<body>
+    <h1>
+    <?php
+    
+    echo "Hello 2210b1";
+
+    ?>
+    </h1>
+
+    <h1>
+        <?php
+        $name="Talha"; //string
+        $age=20; //integer
+        $is_alive=true; //boolean
+        $is_dead=false; //boolean
+        $info="Talha is a good boy";
+        echo $name;
+        echo "<br>";
+        // echo $age;
+        // echo $is_alive;
+        // echo $is_dead;
+        
+        // echo strlen($info);
+// echo str_word_count($info);
+
+// echo strrev($name);
+
+// echo strpos($name,"a");
+
+// echo $name[3];
+
+// echo pi();
+
+// echo abs(-20);
+
+$num1=1000;
+$num2=24;
+
+// $result=sqrt($num1+$num2);
+
+// echo abs($result)+90;
+
+// $result=$num1%$num2; 
+
+// echo $result;
+
+// $result=($num1>$num2);
+// echo $result;
+
+
+
+?>
+    </h1>
+</body>
+</html>
