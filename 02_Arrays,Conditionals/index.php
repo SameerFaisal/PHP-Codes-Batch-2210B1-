f5beb1e1112903ae66db703c6010188d3dc1a7fa
SameerFaisal <sameerfaisal46@gmail.com>
2023-08-04 13:18:31 +0500

    2023-08-04

---
+++ 02_Arrays,Conditionals/index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="alert alert-primary" role="alert">
+<?php
+// $result=(2==="2");
+// $result=(2<>"2");
+// $result=(2!=="2");
+// $age=19;
+// $city="Karachi";
+// $result=(($age>=18)&& ($city!=="Karachi"));
+// $result=(($age>=18)|| ($city!=="Karachi"));
+// echo $result
+
+
+// $names=array("Talha","Ismail","Shahood");
+// echo var_dump($names);
+// echo "<br>";
+// echo var_dump($names[2]);
+
+// $data=array("name"=>"Talha","age"=>20);
+// echo var_dump($data)
+
+// $data=array(array("name"=>"Shahood","age"=>17,"hobby"=>array("Cricket","Football","Sleeping")),array("name"=>"Talha","age"=>20));
+// echo var_dump($data);
+
+// echo sqrt(1024);
+// echo abs(-40)
+// $result=(14+5)/(2-9)*(7+3);
+// echo $result
+// $alphabet="b";
+// if($alphabet==="a"){
+//     echo "Yes";
+// }
+// elseif($alphabet==="e"){
+//     echo "Yes";
+// }
+// elseif($alphabet==="i"){
+//     echo "Yes";
+// }
+// else{
+//     echo  "No";
+// }
+// $age=56;
+// $city="Lahore";
+
+// if($age>=18&&$city==="Karachi"){
+//     echo"You are eligible to vote.";
+// }
+// elseif($age<18&&$city==="Karachi"){
+//     echo"Sorry, you can't vote yet. You must be at least 18 years";
+// }
+// elseif($age>=18&&$city!=="Karachi"){
+//     echo"Congratulations! You have been selected for the next elections in ".$city;
+// }
+// else{
+//     echo"Sorry, you can not vote here!";
+// }
+$num1=9;
+$num2=10;
+$op="*";
+
+// switch($op){
+//     case "+":
+//         $result=$num1+$num2;
+//         echo $result;
+//         break;
+
+//     case "-":
+//         $result=$num1-$num2;
+//         echo $result;
+//         break;
+//     case "*":
+//         $result=$num1*$num2;
+//         echo $result;
+//         break;
+//     case "/":
+//         $result=$num1/$num2;
+//         echo $result;
+//         break;
+//     default:
+//     echo "Invalid Operator";
+        
+// }
+
+
+
+?>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
