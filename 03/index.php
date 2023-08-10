e2ce7b74b31782ed6b933a81e44f0c880ec48a47
Sameer Faisal <sameer@aptech.com>
2023-08-10 11:32:25 +0500

    09-08-2023

---
+++ 03/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container" style="background-color: blue; color: white;">
+<?php
+// $names=array("Mudassir","Shahood","Rabia","Tooba");
+// $data=array("name"=>"Mudassir","age"=>12,"School"=>"Aptech");
+// echo var_dump($names)
+// echo var_dump($data)
+// $i=1;
+// $num=5;
+// while($i<=10){
+// if($i%2===0){
+//     echo $num." x ". $i. " = ". ($i*$num) . "<br>";
+// $i++;
+// }
+// }
+// for($i=1;$i<=10;$i++){
+//     echo $num." x ". $i. " = ". ($i*$num) . "<br>"; 
+// }
+
+
+
+
+
+?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
